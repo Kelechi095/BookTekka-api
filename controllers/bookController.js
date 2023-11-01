45c14ed6463e2fec286174c59c6c10f7c951ca37
Awoms Kelechi <awomskelechi@gmail.com>
2023-11-01 19:13:39 +0100

    updated updateProgress controller

--- controllers/bookController.js
+++ controllers/bookController.js
@@ -76,7 +76,7 @@ export const updateProgress = async (req, res, next) => {
     const { id } = req.params;
     const { currentPage, totalPages } = req.body;
 
-    if (!currentPage, !totalPages) return next(errorHandler(400, "Value required"));
+    if (!currentPage || !totalPages) return next(errorHandler(400, "Value required"));
 
     const newProgress = Math.round((currentPage/totalPages) * 100)
 
