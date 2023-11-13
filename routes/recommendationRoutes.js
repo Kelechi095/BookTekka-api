7637f2a2d62169c79683bb2808ac31f297970007
Awoms Kelechi <awomskelechi@gmail.com>
2023-11-13 16:46:36 +0100

    fixing bugs

--- routes/recommendationRoutes.js
+++ routes/recommendationRoutes.js
@@ -8,6 +8,7 @@ router.route('/').get(getRecommendations)
 router.route('/:id').patch(authenticateUser, likeRecommendation)
 router.route('/:id').post(authenticateUser, createRecommendation)
 router.route('/:id').post(authenticateUser, addToLibrary)
+//router.route("/recommend").post(Recommendation)
 
 
 export default router
